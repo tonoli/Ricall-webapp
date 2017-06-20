a2248fce7f2097111277a2673d9d6bdfc70ab5ab
Ignacio Tonoli de maussion <itonoli-@e1z2r3p25.42.us.org>
2017-06-19 21:47:45 -0700

    clean

--- webapp/loop_file.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/expect
-
-while :
-do
-	echo "Loop to output events running..." &&
-	node quickstart.js > output_file.json &&
-	sshpass -p "pi" scp output_file.json pi@10.10.197.8:/home/pi/Ricall/db/output_file.json
-	sleep 242
-done
